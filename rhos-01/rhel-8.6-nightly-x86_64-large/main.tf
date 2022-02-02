f4a67c6d15c6a31109c7f7619b87bce19378e38d
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2022-02-02 15:08:56 +0100

    openstack: Update rhel-8.6 image-id

--- rhos-01/rhel-8.6-nightly-x86_64-large/main.tf
+++ rhos-01/rhel-8.6-nightly-x86_64-large/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-8-6-nightly-large"
-  image_id  = "c3ab9620-8d58-4af6-b8b6-44ee45514799"
+  image_id  = "e35f742f-0b6d-4bf1-b666-ec1602bd1776"
   flavor_id = "c3ec7a0a-0443-4253-a6ab-040cc0278ced"
 }
 
--- rhos-01/rhel-8.6-nightly-x86_64/main.tf
+++ rhos-01/rhel-8.6-nightly-x86_64/main.tf
@@ -2,7 +2,7 @@ module "openstack" {
   source = "../_base"
 
   name      = "rhel-8-6-nightly"
-  image_id  = "c3ab9620-8d58-4af6-b8b6-44ee45514799"
+  image_id  = "e35f742f-0b6d-4bf1-b666-ec1602bd1776"
   flavor_id = "f2c4469b-f516-46d1-8b87-1dcca68fb3d9"
 }
 
